4a3d4596b113b9e4baba7a14c751c6b8f4546e3f
Faith Makinde <faithmakinde0@gmail.com>
2023-10-08 19:21:48 +0100

    added homepage

--- src/components/healthcare/Navbar.jsx
+++ src/components/healthcare/Navbar.jsx
@@ -102,6 +102,9 @@ export default function Navbar() {
                   </div>
                 </div>
                 <div className="px-2 pt-2 pb-3">
+                <Link href="/"                               className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">
+                   Home 
+                </Link>
                   {navigation.map((item) => (
                     <ScrollLink
                     key={item.name}
